1af627c411cd291db71c8e107f0a4938e663e9e6
Cristian Stinca <crististinca.work@gmail.com>
2024-08-30 17:27:37 +0200

    optimised dependencies

--- CarRental/src/main/java/com/crististinca/CarRental/model/Rents.java
+++ CarRental/src/main/java/com/crististinca/CarRental/model/Rents.java
@@ -1,31 +1,19 @@
 package com.crististinca.CarRental.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDate;
 
 @Data
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class Rents {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "car_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Car car;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "client_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Client client;
 
     private LocalDate rentalDateStart;
